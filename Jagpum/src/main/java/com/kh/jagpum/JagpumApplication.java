c9a7989f7794274bc0d276cebd497405fd391870
wlwldnjs1004 <wiwiw1009@naver.com>
2025-04-21 19:52:26 +0900

    프로잭트 파일 만들기

---
+++ Jagpum/src/main/java/com/kh/jagpum/JagpumApplication.java
@@ -0,0 +1,13 @@
+package com.kh.jagpum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagpumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagpumApplication.class, args);
+	}
+
+}
---
+++ Jagpum/src/test/java/com/kh/jagpum/JagpumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.jagpum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagpumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
