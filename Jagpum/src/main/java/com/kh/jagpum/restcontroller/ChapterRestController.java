076b04774a251300f2e54a167c7b1d873fb753ec
wlwldnjs1004 <wiwiw1009@naver.com>
2025-04-28 20:59:57 +0900

    이미지 디비 저장 올리기

--- Jagpum/src/main/java/com/kh/jagpum/restcontroller/ChapterRestController.java
+++ Jagpum/src/main/java/com/kh/jagpum/restcontroller/ChapterRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.jagpum.dao.ChapterDao;
@@ -17,9 +18,9 @@ import com.kh.jagpum.service.AttachmentService;
 
 
 @CrossOrigin
+@RestController
 @RequestMapping("/rest/chapter")
 public class ChapterRestController {
-
 	
 	@Autowired
 	private ChapterDao chapterDao;
@@ -28,22 +29,25 @@ public class ChapterRestController {
 	private AttachmentService attachmentService;
 	
 	@PostMapping("/upload")
-	public AttachmentDto upload(@RequestParam MultipartFile attach) throws IllegalStateException, IOException {
-		if(attach.isEmpty()) {
-		}
-		return attachmentService.save(attach);
+	public List<Integer> upload(@RequestParam MultipartFile attach) throws IllegalStateException, IOException {
+	    List<Integer> list = new ArrayList<>();
+	    if (!attach.isEmpty()) {
+	        AttachmentDto result = attachmentService.save(attach);
+	        list.add(result.getAttachmentNo());
+	    }
+	    return list;
 	}
+
 	
 	@PostMapping("/uploads")
-	public List<Integer> uploads(@RequestParam(value="attach")List<MultipartFile>attachList) throws IllegalStateException, IOException{
-	List<Integer> numbers=new ArrayList<>();
-	for(MultipartFile attach:attachList) {
-		if(attach.isEmpty()) continue;
-		AttachmentDto attachmentNo=attachmentService.save(attach);
-		numbers.add(attachmentNo.getAttachmentNo());
-	}
-	
-	return numbers;
+	public List<Integer> uploads(@RequestParam(value="attach") List<MultipartFile> attachList) throws IllegalStateException, IOException {
+		List<Integer> numbers = new ArrayList<>();
+		for(MultipartFile attach : attachList) {
+			if(attach.isEmpty()) continue;
+			AttachmentDto attachmentNo = attachmentService.save(attach);
+			numbers.add(attachmentNo.getAttachmentNo());
+		}
+		return numbers;
 	}
 	
 }
